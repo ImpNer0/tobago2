d52d3846097590e1aaedbb5f722f602003ecf598
rohsik2 <rohsik2@gmail.com>
2023-08-28 23:08:39 +0900

    feat : smokingZone확인및 현위치 확인을 LatLng 객체를 통해 하도록 변경,

--- app/src/main/java/com/example/tabago/LocationUtility.java
+++ app/src/main/java/com/example/tabago/LocationUtility.java
@@ -1,6 +1,5 @@
 package com.example.tabago;
 
-import android.app.Activity;
 import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
@@ -8,31 +7,27 @@ import android.os.Bundle;
 
 import com.naver.maps.geometry.LatLng;
 import com.naver.maps.map.overlay.Marker;
-import com.naver.maps.map.util.FusedLocationSource;
 
 import java.util.List;
 
 public class LocationUtility {
-    public static boolean isSmokingZone(Activity activity, SubActivity3 subActivity3) {
-        List<Marker> lstMarkers = subActivity3.getLstMarkers(); // lstMarkers 변수에 접근
-        FusedLocationSource fusedLocationSource = new FusedLocationSource(activity, SubActivity3.LOCATION_PERMISSION_REQUEST_CODE);
-        LatLng currentLocation = new LatLng(fusedLocationSource.getLastLocation().getLatitude(),
-                fusedLocationSource.getLastLocation().getLongitude());
 
-        for (Marker marker : lstMarkers) {
-            LatLng markerLocation = marker.getPosition();
-            double distance = calculateDistance(currentLocation, markerLocation);
+    private static List<LatLng> smokingCoordinates;
 
+    // 확인하고자 하는 위치(사용자의 현위치)의 좌표를 넣으면, 마커들 안에 있는지 확인함.
+    public static boolean isSmokingZone(LatLng latLng) {
+        List<LatLng> smokingCoordinates = LocationUtility.getSmokingCoordinates();
+        for (LatLng marker : smokingCoordinates) {
+            double distance = calculateDistance(latLng, marker);
             // 일정 거리 이내에 마커가 존재하면 SmokingZone으로 판단
             if (distance < 100) { // 예시로 100 미터 이내로 설정
                 return true;
             }
         }
-
         return false;
     }
 
-    static double calculateDistance(LatLng point1, LatLng point2) {
+    public static double calculateDistance(LatLng point1, LatLng point2) {
         double lat1 = point1.latitude;
         double lon1 = point1.longitude;
         double lat2 = point2.latitude;
@@ -47,7 +42,10 @@ public class LocationUtility {
         dist = dist * 60 * 1.1515 * 1.609344 * 1000; // 거리를 미터로 변환
 
         return dist;
-    } public Location getCurrentLocation(Context context) {
+    }
+
+    // 현 위치를 LatLng 객체로 반환한다.
+    public static LatLng getCurrentLocation(Context context) {
         GPSTracker gpsTracker = new GPSTracker(context);
         LocationListener locationListener = new LocationListener() {
             @Override
@@ -75,7 +73,26 @@ public class LocationUtility {
         // GPS 위치 추적 중지
         gpsTracker.stopUsingGPS(locationListener);
 
-        return currentLocation;
+        return new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
     }
 
+
+    public static List<LatLng> getSmokingCoordinates(){
+        if(smokingCoordinates != null)
+            return smokingCoordinates;
+
+        smokingCoordinates = List.of(
+                new LatLng(37.210279, 126.975302),
+                new LatLng(37.211645, 127.980015),
+                new LatLng(37.212269, 127.970126),
+                new LatLng(37.390935, 126.956417),
+                new LatLng(37.391317, 126.957767),
+                new LatLng(37.390935, 126.956417),
+                new LatLng(37.398283, 126.952497),
+                new LatLng(37.397808, 126.953833),
+                new LatLng(37.411986, 126.951515)
+        );
+
+        return smokingCoordinates;
+    }
 }
